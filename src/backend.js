8d9f151877d705853aea23d426d2bf1d3d74eeb1
Davide Cossidente <davidecossidente03@gmail.com>
2025-03-26 12:06:52 +0100

    Now saving the flag value with entity-extensions in YouTrack backend

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,4 @@
+/*
 // Cache for projects
 const projectCache = new Map();
 
@@ -53,7 +54,66 @@ exports.httpHandler = {
         
         project.flag = flag;
         projectCache.set(id, project);
-        
+        return ctx.response.json({proj: project, flag: flag});
+      } catch (error) {
+        return ctx.response.json({error: error.message});
+      }
+    }
+  }
+  ]
+};*/
+const entities = require('@jetbrains/youtrack-scripting-api/entities');
+
+exports.httpHandler = {
+  endpoints: [
+    {
+      method: 'GET',
+      path: 'project',
+      handle: function handle(ctx) {
+        try {
+          const name = ctx.request.getParameter('name');
+          const projectFlag = entities.Project.findByName(name).extensionProperties.flag;
+          return ctx.response.json({flag: projectFlag});
+        } catch (error) {
+          return ctx.response.json({error: error.message});
+        }
+      }
+    },
+    {
+      method: 'POST',
+      path: 'projects',
+      handle: function handle(ctx) {
+        try {
+          const projects = JSON.parse(ctx.request.body);
+          for (const project of projects) {
+            if (project.id && project.name) {
+              entities.Project.findByName(project.name).extensionProperties.flag = project.flag || false;
+            }
+          }
+          return ctx.response.json({"success": "true"});
+        } catch (error) {
+          return ctx.response.json({error: error.message});
+        }
+      }
+    },
+    {
+      method: 'PUT',
+      path: 'projects/flag',
+      handle: function handle(ctx) {
+      try {
+        const name = ctx.request.getParameter('name')
+        const parsedBody = JSON.parse(ctx.request.body);
+        const flag = parsedBody.flag;
+
+        if (typeof flag !== 'boolean') {
+          return ctx.response.json({error: 'Invalid flag value'});
+        }
+
+        const project = entities.Project.findByName(name);
+        if (!project) {
+          return ctx.response.json({error: 'Project not found'});
+        }
+        entities.Project.findByName(name).extensionProperties.flag = flag;
         return ctx.response.json({proj: project, flag: flag});
       } catch (error) {
         return ctx.response.json({error: error.message});
